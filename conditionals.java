622fdf44bb592ab386cfac5c77f5186d7d34a30b
SuryaJ-svg <surya.jeeda@gmail.com>
2024-08-07 19:48:20 -0400

    Add files via upload

---
+++ conditionals.java
@@ -0,0 +1,11 @@
+public class conditionals {
+    public static void main(String[] args) throws Exception {
+        double x = 99;
+        if (x > 100) {
+            System.out.println(x/20);
+        }
+        else {
+            System.out.println(x+50);
+        }
+        }
+    }
---
+++ operators.java
@@ -0,0 +1,12 @@
+public class operators {
+    public static void main(String[] args) throws Exception {
+
+        int x = 5;
+        int y = 5;
+        x++;
+        y--;
+        int z = x + y;
+        System.out.println(z);
+
+}
+}
